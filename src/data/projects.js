676b9eca670bc3185efea9b568756ec454f6c299
Maryery Vargas Moreno <maryeryvargas@gmail.com>
2021-08-06 18:28:33 +0100

    add changes projects name

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ import hboImg from '../assets/images/hbo.png';
 
 const PROJECTS_INFO = [
 	{
-		title: 'HBO',
+		title: 'COMMING SOON',
 		description:
 			'El objetivo de esta practica era realizar una plataforma de streaming de contenido digital similiar a HBO, para distribuir series y peliculas, solo con HTML5 y CSS, sin el uso de librerías y frameworks externos, entre otros detalles, el diseño debe ser responsivo',
 		githubLink: 'https://github.com/Maryery/practica-html-css-github',
@@ -11,11 +11,11 @@ const PROJECTS_INFO = [
 		skills: ['HTML', 'CCS'],
 	},
 	{
-		title: 'HBO',
+		title: 'POKEMON BAY',
 		description:
-			'El objetivo de esta practica era realizar una plataforma de streaming de contenido digital similiar a HBO, para distribuir series y peliculas, solo con HTML5 y CSS, sin el uso de librerías y frameworks externos, entre otros detalles, el diseño debe ser responsivo',
-		githubLink: 'https://github.com/Maryery/practica-html-css-github',
-		webLink: 'https://maryery.github.io/html-css/index.html',
+		'Encuentra tu pokemon favorito',
+		githubLink: 'https://github.com/Maryery/pokemon-bay',
+		webLink: 'http://pokemon.beatrizvargasmoreno.com',
 		img: hboImg,
 		skills: ['HTML', 'CCS'],
 	},
