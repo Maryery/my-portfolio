9e5ba0c7552cd039be865850989b01e3738de718
Maryery Vargas Moreno <maryeryvargas@gmail.com>
2021-08-10 22:40:34 +0100

    Add more information about my projets

--- src/components/aboutMe/aboutMe.jsx
+++ src/components/aboutMe/aboutMe.jsx
@@ -15,9 +15,9 @@ const AboutMe = () => {
 				<S.AboutMeTextContainer>
 					<S.TextTitle>¡Hola!</S.TextTitle>
 					<S.AboutMeParagraph>
-						Mi nombre es <span>Beatriz</span>. Soy ingeniera industrial, con
-						años de experiencia en el sector óptico y ahora Full stack web
-						student. Decidí volver a mis orígenes de ingeniera y dar un giro de
+						Mi nombre es <span>Maryery</span>. Soy ingeniera industrial,
+						con años de experiencia en el sector retail y ahora Junior Sotfware
+						Engineer. Decidí volver a mis orígenes de ingeniera y dar un giro de
 						180 grados, buscando un reto profesional motivador y dinámico a
 						través del bootcamp de desarrollo full stack web de Keepcoding. Soy
 						una desarrolladora en progreso, amante de la tecnología y de
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,42 +1,43 @@
 import hboImg from '../assets/images/hbo.png';
+import commingSoon from '../assets/images/comming.jpg';
+import pokemonBay from '../assets/images/pokemonBay.png';
+import wallaclone from '../assets/images/wallaclone.png';
 
 const PROJECTS_INFO = [
 	{
-		title: 'COMMING SOON',
+		title: 'Coming Soon!',
 		description:
-			'El objetivo de esta practica era realizar una plataforma de streaming de contenido digital similiar a HBO, para distribuir series y peliculas, solo con HTML5 y CSS, sin el uso de librerías y frameworks externos, entre otros detalles, el diseño debe ser responsivo',
-		githubLink: 'https://github.com/Maryery/practica-html-css-github',
-		webLink: 'https://maryery.github.io/html-css/index.html',
-		img: hboImg,
-		skills: ['HTML', 'CCS'],
+			'Encuentra tu destino de viaje y toda la infomación que debes saber acerca de las restricciones COVID-19',
+		githubLink: '',
+		img: commingSoon,
+		skills: ['REACT'],
 	},
 	{
-		title: 'POKEMON BAY',
+		title: 'Wallaclone',
 		description:
-		'Encuentra tu pokemon favorito',
+		'Es una plataforma que permite la compra y venta de productos de segunda mano, similar a aplicaciones como wallapop, ebay,etc',
+		githubLink: 'https://github.com/Maryery/wallaclone.git',
+		img: wallaclone,
+		skills: ['MONGO', 'EXPRESS', 'REACT', 'NODE'],
+	},
+	{
+		title: 'Pokémon Bay',
+		description:
+		'Es una aplicación web, que utiliza LA RESTful API de Pokémon (http://pokeapi.co/), la cual te permite encontrar tu pokemon favorito',
 		githubLink: 'https://github.com/Maryery/pokemon-bay',
 		webLink: 'http://pokemon.beatrizvargasmoreno.com',
-		img: hboImg,
-		skills: ['HTML', 'CCS'],
+		img: pokemonBay,
+		skills: ['REACT'],
 	},
 	{
 		title: 'HBO',
 		description:
-			'El objetivo de esta practica era realizar una plataforma de streaming de contenido digital similiar a HBO, para distribuir series y peliculas, solo con HTML5 y CSS, sin el uso de librerías y frameworks externos, entre otros detalles, el diseño debe ser responsivo',
+		'Es una plataforma de streaming de contenido digital similiar a HBO, para distribuir series y peliculas, solo con HTML5 y CSS, sin el uso de librerías y frameworks externos, entre otros detalles.',
 		githubLink: 'https://github.com/Maryery/practica-html-css-github',
 		webLink: 'https://maryery.github.io/html-css/index.html',
 		img: hboImg,
-		skills: ['HTML', 'CCS'],
-	},
-	{
-		title: 'HBO',
-		description:
-			'El objetivo de esta practica era realizar una plataforma de streaming de contenido digital similiar a HBO, para distribuir series y peliculas, solo con HTML5 y CSS, sin el uso de librerías y frameworks externos, entre otros detalles, el diseño debe ser responsivo',
-		githubLink: 'https://github.com/Maryery/practica-html-css-github',
-		webLink: 'https://maryery.github.io/html-css/index.html',
-		img: hboImg,
-		skills: ['HTML', 'CCS'],
-	},
+		skills: ['HTML5', 'CCS3'],
+	}
 ];
 
 export default PROJECTS_INFO;
