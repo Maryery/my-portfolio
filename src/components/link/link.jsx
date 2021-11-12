532213f46875469f9ae105724650497b07945492
Maryery Vargas Moreno <maryeryvargas@gmail.com>
2021-11-12 15:05:22 +0000

    update cv

--- src/components/link/link.jsx
+++ src/components/link/link.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Icon } from 'semantic-ui-react';
 import * as S from './styles';
-import CVBeatriz from '../../assets/documents/Beatriz.pdf'
-import CVBeatrizEnglish from '../../assets/documents/Maryery_EV81.pdf'
+import CVBeatriz from '../../assets/documents/MaryeryVargasCV.pdf'
 
 const Link = () => {
 	return (
 		<S.LinkContainer>
 			<S.SocialLink href={CVBeatriz} download="CVBeatriz">
 				<Icon name="file pdf" /> CV Español
-			</S.SocialLink>
-			<S.SocialLink href={CVBeatrizEnglish} download="CVBeatrizEnglish">
-				<Icon name="file pdf" /> CV English
 			</S.SocialLink>
 			<S.SocialLink href="https://www.github.com/Maryery" target="_blank">
 				<Icon name="github" /> GitHub
